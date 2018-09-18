67d73f2fbeab1a4779e1c87412b5949ca688af59
Joy Vivika <joiemumo@gmail.com>
2018-09-18 17:03:02 +0300

    linked framework with html

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+       $("button#grape").click(function() {
+         $("body").addClass("green-background");
+       });
+
+       $("button#wine").click(function() {
+         $("body").addClass("yellow-background");
+       });
+
+     });
